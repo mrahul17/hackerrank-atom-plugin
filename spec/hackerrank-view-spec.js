8acf14eb2ebfd17e8d07f6ae4d7e2ade90a65093
Rahul Mishra <mishra.rahul1712@gmail.com>
2017-07-05 01:17:23 +0530

    Initial commit

---
+++ spec/hackerrank-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HackerrankView from '../lib/hackerrank-view';
+
+describe('HackerrankView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
